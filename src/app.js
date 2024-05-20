1105c283bc6d31d27dffe215436cd782b262ab32
RealRichi3 <molunorichie@gmail.com>
2024-05-20 13:00:31 +0100

    update file max size

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ const app = express();
 const ORIGIN = process.env.ALLOWED_ORIGINS.split(",");
 console.log({ ORIGIN });
 app.use(morgan("dev"));
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
